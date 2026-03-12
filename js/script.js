039fe5aace6fb729fd2bd676d0e4428aa8b2542f
dpysartsevart-gif <dpysartsevart@gmail.com>
2026-03-12 14:03:48 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1127,7 +1127,7 @@ function processCommand(cmd) {
                 audioToggle.innerText = "[ AUDIO : OFF ]";
                 audioToggle.classList.add('muted');
             } else {
-                audioToggle.innerText = "[ AUDIO : OnN  ]";
+                audioToggle.innerText = "[ AUDIO : ON  ]";
                 audioToggle.classList.remove('muted');
                 safePlay('snd-select'); 
             }
